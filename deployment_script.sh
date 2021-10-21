481e4e127ecd03a3400a9a5d05e3f5cf3bddfad6
johnmogi <anguru@gmail.com>
2021-10-21 09:48:07 +0300

    t2

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,8 +1,8 @@
 cd /
+sudo git init
+sudo git pull https://github.com/johnmogi/vagrant.git
 sudo python3 -m venv venv
  source venv/bin/activate
 sudo pip install Flask
-git init
-git pull https://github.com/johnmogi/vagrant.git
 export FLASK_APP=webapp
 flask run
